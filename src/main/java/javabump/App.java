84baf4453151f240f3cee3dea96deb205a38b8de
Arek Burdach <arek.burdach@gmail.com>
2021-03-18 20:16:05 +0100

    lambda

--- src/main/java/javabump/App.java
+++ src/main/java/javabump/App.java
@@ -15,10 +15,12 @@
  */
 package javabump;
 
+import java.util.stream.IntStream;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("WORKING");
+        IntStream.range(0, 3).forEach((i) -> System.out.println("WORKING"));
     }
 
 }
